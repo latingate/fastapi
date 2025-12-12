a9a108148ba64f78c75db2ac8c689bf843fabbd0
Gal Sarig <gal2016@latingate.com->
2025-12-12 20:02:26 +0200

    Edit from Android

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, Gal!", "message": "Welcome to FastAPI! I've changed this line :)"}
+    return {"greeting": "Hello, Gal Sarig 2!", "message": "Welcome to FastAPI! I've changed this line :)"}
